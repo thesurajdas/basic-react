c2a937d3df604407558d25578b0f951f9aa60af1
Suraj Das <blogwapz@gmail.com>
2021-11-15 16:55:33 +0530

    Update React Router 6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" about="About" />
-      <TextForm heading="Enter The Text" />
-      {/* <About/> */}
+      <Router>
+        <Navbar title="TextUtils" about="About" />
+        <Routes>
+          <Route path="/about" element={<About />}>
+          </Route>
+          <Route path="/" element={<TextForm heading="Enter The Text" />}>
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function About() {
+    const [myStyle, setmyStyle] = useState({
+        color: 'black',
+        backgroundColor: 'white'
+    })
+
+    const [btnText, setBtnText] = useState("Enable Dark Mode")
+
+    const toggleStyle = () => {
+        if (myStyle.color === 'white') {
+            setmyStyle({
+                color: 'black',
+                backgroundColor: 'white'
+            })
+            setBtnText("Enable Dark Mode")
+        }
+        else {
+            setmyStyle({
+                color: 'white',
+                backgroundColor: 'black'
+            })
+            setBtnText("Enable Light Mode")
+        }
+    }
+
     return (
         <>
             <div className="container my-2">
                 <h1>About Us</h1>
-                <div className="card">
+                <div className="card" style={myStyle}>
                     <div className="card-body">
                         This is some text within a card body.
                     </div>
                 </div>
+            </div>
+            <div className="container my-3">
+                <button className="btn btn-primary" onClick={toggleStyle}>{btnText}</button>
             </div>
         </>
     )
